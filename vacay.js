fd25831b001dbd69cd8260fc6553d0a9d2f81b13
Azriy-Nazariee <1211104288@student.mmu.edu.my>
2024-01-26 16:50:05 +0800

    Update HTML files and CSS styles

--- vacay.js
+++ vacay.js
@@ -1,8 +1,8 @@
-document.getElementById('menuButton').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display === 'none') {
-        menu.style.display = 'block';
+document.getElementById('menu-button').addEventListener('click', function() {
+    var navMenu = document.getElementById('nav-menu');
+    if (navMenu.style.display === 'none') {
+        navMenu.style.display = 'block';
     } else {
-        menu.style.display = 'none';
+        navMenu.style.display = 'none';
     }
 });
